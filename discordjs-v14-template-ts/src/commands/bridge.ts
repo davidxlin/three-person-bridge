ed39f9f2d202bfad589fc0124dc2d7b06066dd45
David Lin <davidxlin11@gmail.com>
2023-11-29 19:49:48 -0800

    add help string

--- discordjs-v14-template-ts/src/commands/bridge.ts
+++ discordjs-v14-template-ts/src/commands/bridge.ts
@@ -4,15 +4,16 @@ import { Command } from "../types";
 const command : Command = {
     name: "bridge",
     execute: (message, args) => {
+        const helpString = 'See https://github.com/davidxlin/three-person-bridge for help.'
         const board = require("../bridge/board").default
         if (args.length < 2) {
-            message.channel.send(`This call requires a parameter!`)
+            message.channel.send(`This call requires a parameter! ${helpString}`)
             return
         } else if (args[1] == "newgame") {
             board.shuffle()
         } else if (args[1] == "diagram") {
             if (args.length < 3) {
-                message.channel.send(`This call requires the declarer to be entered as a parameter!`)
+                message.channel.send(`This call requires the declarer to be entered as a parameter! ${helpString}`)
             }
             try {
                 message.channel.send(`\`\`\`${board.diagram(args[2])}\`\`\``)
@@ -21,7 +22,7 @@ const command : Command = {
             }
             return
         } else if (!board.handOptions.includes(args[1])) {
-            message.channel.send(`Invalid parameter!`)
+            message.channel.send(`Invalid parameter! ${helpString}`)
             return
         }
         let hands = board.handOptions.filter((option: string) =>
