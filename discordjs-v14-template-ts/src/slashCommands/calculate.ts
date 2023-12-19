b0d30ced344b4ab3c8ad439f822fa6c629e911e3
David Lin <davidxlin11@gmail.com>
2023-12-18 17:32:27 -0800

    remove calculate command

--- discordjs-v14-template-ts/src/slashCommands/calculate.ts
+++ discordjs-v14-template-ts/src/slashCommands/calculate.ts
@@ -5,59 +5,6 @@ import scoreKeeper from "../bridge/calculator/ScoreKeeper"
 
 const updateScoresReplies: (() => Promise<Message<boolean>>)[] = []
 
-const calculateCommand : SlashCommand = {
-    command: new SlashCommandBuilder()
-    .setName("calculate")
-    .setDescription("Calculates the score of a bridge contract.")
-    .addIntegerOption(option => {
-        return option
-        .setMaxValue(7)
-        .setMinValue(1)
-        .setName("level")
-        .setDescription("Level of the contract.")
-        .setRequired(true)
-    })
-    .addStringOption(option => {
-        return option
-        .setName("strain")
-        .setDescription("Strain of the contract.")
-        .setRequired(true)
-    })
-    .addIntegerOption(option => {
-        return option
-        .setMaxValue(7)
-        .setMinValue(-13)
-        .setName("tricks")
-        .setDescription("Odd tricks made or down.")
-        .setRequired(true)
-    })
-    .addStringOption(option => {
-        return option
-        .setName("doubled")
-        .setDescription("Doubled state of the contract.")
-    })
-    .addStringOption(option => {
-        return option
-        .setName("vulnerability")
-        .setDescription("Vulnerability of declaring side.")
-    }),
-    execute: interaction => {
-        const level = Number(interaction.options.get("level")!.value)
-        const strain = String(interaction.options.get("strain")!.value)
-        const tricksMadeOrDown = Number(interaction.options.get("tricks")!.value)
-        const doubledState = interaction.options.get("doubled")?.value as string | undefined
-        const vulnerability = interaction.options.get("vulnerability")?.value as string | undefined
-
-        const invalidReason = validateInput(level, strain, tricksMadeOrDown, doubledState, vulnerability)
-        if (invalidReason) {
-            interaction.reply(invalidReason)
-        } else {
-            const contract = createUnpassedContractUnchecked(level, strain, tricksMadeOrDown, doubledState, vulnerability)
-            interaction.reply(`Score: ${contract.score()}`)
-        }
-    },
-}
-
 const addScoreCommand : SlashCommand = {
     command: new SlashCommandBuilder()
     .setName("addscore")
@@ -170,7 +117,6 @@ const removeLastScoreCommand: SlashCommand = {
 }
 
 const calculateCommands = [
-    calculateCommand,
     addScoreCommand,
     scoresCommand,
     addPassScoreCommand,
